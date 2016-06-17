1496d1f936390cb555ce0f70f15347e3f534d535
yuxizhang <zhangyxwings@gmail.com>
2016-06-16 21:43:22 -0700

    add naive ai test

--- src/bacteria simulation/bacteria simulation/FANN.cpp
+++ src/bacteria simulation/bacteria simulation/FANN.cpp
@@ -7,8 +7,8 @@ void FANN_Train(string datafile, string netfile)
 {
     const unsigned int num_input = 31;
     const unsigned int num_output = 1;
-    const unsigned int num_layers = 5;
-    const unsigned int num_neurons_hidden = 5;
+    const unsigned int num_layers = 3;
+    const unsigned int num_neurons_hidden = 3;
     const float desired_error = (const float) 0.001;
     const unsigned int max_epochs = 50000;
     const unsigned int epochs_between_reports = 1000;
